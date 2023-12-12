a6949c64f1081a706b5078a6dd3e3f4309cc28a4
Samriddha Saha <121436784+Sam2621@users.noreply.github.com>
2023-12-12 15:09:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,22 @@ elif(num2 > num1 and num2 > num3) :
   ans = num2
 elif(num3>num1 and num3>num2):
   ans = num3
+elif(num1==num2):
+  if(num1 > num3):
+    ans = num1
+  else:
+    ans = num3
+elif(num2==num3):
+  if(num2 > num1):
+    ans = num2
+  else:
+    ans = num1
+elif(num1==num3):
+  if(num1 > num2):
+    ans = num1
+  else:
+    ans = num2
+elif(num1==num2==num3):
+  ans = num1
 st.subheader("The largest number")
 st.write(ans)
