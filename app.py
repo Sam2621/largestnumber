66648c638182b5f3d9713324b6e7d184492c6d8b
Samriddha Saha <121436784+Sam2621@users.noreply.github.com>
2023-12-12 14:56:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+st.write("""
+# Largest number 
+
+This app predicts the largest of the given 3 numbers
+""")
+st.header('User input numbers')
+num1 = st.number_input('FIRST NUMBER')
+num2 = st.number_input('SECOND NUMBER')
+num3 = st.number_input('THIRD NUMBER')
+
+  # data = {'FIRST NUMBER' : num1,
+  #         'SECOND NUMBER' : num2,
+  #         'THIRD NUMBER' : num3
+  #         }
+ans = 0
+if(num1>num2 and num1>num3) :
+  ans = num1
+elif(num2 > num1 and num2 > num3) :
+  ans = num2
+elif(num3>num1 and num3>num2):
+  ans = num3
+st.subheader("The largest number")
+st.write(ans)
